cca7b6f8c93cc4eb4107b2941dace92a545e5c8b
unknown <92856544+RM35@users.noreply.github.com>
2022-09-05 23:07:58 +0100

    fix: remove redundant commented code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pytesseract import Output
 import re
 import datetime
 
-image = cv2.imread('res/ss2.png', 0)
+image = cv2.imread('res/ss.png', 0)
 wid = image.shape[1]
 height = image.shape[0]
 
@@ -29,15 +29,7 @@ race_name_maxx = wid * 0.400
 race_name_miny = height * 0.104
 race_name_maxy = height * 0.139
 
-# print(f"{wid} + {height}")
-# scale_percent = 75 # percent of original size
-# width = int(image.shape[1] * scale_percent / 100)
-# height = int(image.shape[0] * scale_percent / 100)
-# dim = (width, height)
-# image = cv2.resize(image, dim)
-
 image = cv2.threshold(image, 220, 255, cv2.THRESH_BINARY)[1]
-
 
 d = pytesseract.image_to_data(image, output_type=Output.DICT)
 image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
@@ -89,27 +81,6 @@ for i, text in enumerate(d['text']):
             image = cv2.rectangle(
                 image, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-# # Player Names
-# alphanumeric = re.compile(r'[a-zA-Z0-9]')
-# for i, text in enumerate(d['text']):
-#     if alphanumeric.match(text):
-#         (x, y, w, h) = (d['left'][i], d['top']
-#                         [i], d['width'][i], d['height'][i])
-#         if x > name_minx and x < name_maxx and d['conf'][i] > 0.8 and y > table_start_y:
-#             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-# # Car types
-# letter = re.compile(r'[a-zA-Z]')
-# for i, text in enumerate(d['text']):
-#     if letter.match(text):
-#         (x, y, w, h) = (d['left'][i], d['top']
-#                         [i], d['width'][i], d['height'][i])
-#         if x > car_minx and x < car_maxx and y > table_start_y:
-#             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 0, 255), 2)
-#             print(text)
-#             print(
-#                 f"{d['level'][i]} {d['line_num'][i]} {d['conf'][i]} {d['block_num'][i]}")
-
 # # debug:::: Grid the image for measuring where the table lies
 # x_locs = [x for x in range(0, 2559, 100)]
 # y_locs = [x for x in range(0, 1439, 100)]
@@ -126,10 +97,9 @@ for i, text in enumerate(d['text']):
 
 timenow = datetime.datetime.now().strftime("%d.%m.%y %H.%M.%S")
 filename = racename + timenow
-# cv2.imshow('img', image)
-# cv2.waitKey(0)
+
 cv2.imwrite(f"res/{filename}.png", image)
 
 with open(f"res/{filename}.txt", "w", encoding="utf-8") as f:
     for record in times:
-        f.write(f"{record[0]}\t{record[1]}\t{record[2]}\n")
+        f.write(f"{record[0]},{record[1]},{record[2]}\n")
